057903b8b8f57d2d709520c0a6f9ce1923689ecd
mochafri <afrizalmuhammad656@gmail.com>
2026-06-13 19:55:18 +0700

    feat: initialize App component with animated background and project sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Project from "./assets/components/Project"
 import Contact from "./assets/components/Contact"
 import Footer from "./assets/components/Footer"
 import Navbar from "./assets/components/Navbar"
-import Cursor from "./assets/components/Cursor"
 
 function App() {
 
@@ -18,7 +17,6 @@ function App() {
         <div className="absolute top-[40%] left-[30%] w-[300px] sm:w-[400px] h-[300px] sm:h-[400px] rounded-full bg-[#0EA5E9]/20 blur-[100px] sm:blur-[120px] animate-blob3"></div>
       </div>
 
-      <Cursor />
       <Navbar />
       <Hero />
       <AboutMe />
